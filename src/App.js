3602a305d70c95c80e3cbd761c456f4a3c9a07ed
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-09 11:31:42 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import TodosContainer from './TodosContainer';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello World! </h1>
+        {/* <h1> Hello World! </h1> */}
+        <Switch>
+            <Route exact path='/' component={ Home }/>
+            <Route path='/todos' component={ TodosContainer }/>
+        </Switch>  
       </div>
     );
   }
