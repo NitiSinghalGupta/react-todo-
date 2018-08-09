769763d926cc0dcef091bf5ea6884a467021c727
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-09 15:34:54 -0700

    sprint 4

--- src/Container/TodosContainer.js
+++ src/Container/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import Todos from '../Components/Todos';
+import CreateTodoForm from '../Components/CreateTodoForm';
 
 export default class TodosContainer extends Component {
   constructor(){
@@ -8,6 +9,7 @@ export default class TodosContainer extends Component {
     this.state = {
       todos: []
     }
+    this.createTodo = this.createTodo.bind(this);
   }
 
   componentDidMount = () => {
@@ -25,13 +27,26 @@ export default class TodosContainer extends Component {
     .catch(err => console.log(err))
   }
 
+  createTodo(todo) {
+    let newTodo = {
+        body: todo,
+        completed: false
+    }
+    TodoModel.create(newTodo).then((res) => {
+        let todos = this.state.todos
+        let newTodos = todos.push(res.data)
+        this.setState({newTodos})
+    })
+}
 
   render() {  
   
     return (
-      <div className="TodosContainer" >
+      <div className="todosComponent" >
         <h1> Welcome to my Todos Page! </h1>
         <Todos  todos={this.state.todos}   />
+        <CreateTodoForm
+        createTodo={ this.createTodo } />
       </div>
     );
   }
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -6,4 +6,8 @@ export default class TodoModel {
         let requestUrl = axios.get("https://super-crud-api.herokuapp.com/api/todos");
         return requestUrl
     }
+    static create(todo) {
+        let request = axios.post("https://super-crud-api.herokuapp.com/api/todos", todo)
+        return request
+      }
 }
