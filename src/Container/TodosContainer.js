96aca24a02581d2c203381dd8e3bbaf6fb9c8df7
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-09 11:55:09 -0700

    creating sep folders for comp n cont

---
+++ src/Container/TodosContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class TodosContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Welcome to my Home Page! </h1>
+      </div>
+    );
+  }
+}
